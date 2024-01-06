4d6a3bf2e0be4e9744b069b61ebbb70db37d31fc
D3Y4N <103333790+Deyan2306@users.noreply.github.com>
2024-01-06 22:25:13 +0200

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+
+const int INF = 1e9;
+
+/* May the source be with you */
+int main(void) {
+
+    return 0;
+}
