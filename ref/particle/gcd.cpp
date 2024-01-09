fe11c2920e7455175c007b513d53a7be69c7cde1
D3Y4N <103333790+Deyan2306@users.noreply.github.com>
2024-01-09 08:38:06 +0200

    Create gcd.cpp

---
+++ ref/particle/gcd.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+#define LL long long
+
+// Greatest Common Divisor using the Euclidean Algorithm
+// 1. Checks base Cases
+// 2. Uses Recurrance if there is a difference between the numbers, and neither one of the numbers is 0
+// It works on the principle that the GCD of two numbers remains the same if the greater number is replaced by the difference between the two numbers.
+LL gcd(LL a, LL b) {
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a;
+
+    if (a == b)
+        return a;
+    
+    if (a > b)
+        return gcd(a - b, b);
+    return gcd(a, b - a);
+}
