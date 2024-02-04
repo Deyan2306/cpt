eb6723080f70133aef15f8743c67cc3286970d26
D3Y4N <103333790+Deyan2306@users.noreply.github.com>
2024-02-04 22:07:32 +0200

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -6,8 +6,15 @@ typedef vector<int> vi;
 
 const int INF = 1e9;
 
+// Output handling
+stringstream output;
+void add_to_out(const string& str) {
+    output << str;
+}
+
 /* May the source be with you */
 int main(void) {
 
+    cout << output.str();
     return 0;
 }
